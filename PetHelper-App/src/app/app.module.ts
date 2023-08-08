f9dc5614f68fa3340d74f7f8b94304ae494a3572
Tallys Witer <tallyswiter@hotmail.com>
2023-08-08 04:25:38 -0300

    Adicionando dependências ao projeto.
    Adicionando bootstrap, ngx-bootstrap e fortawesome ao projeto.

--- PetHelper-App/src/app/app.module.ts
+++ PetHelper-App/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
