53d97574246f2fae62e419206ae42ed677b30933
Tallys Witer <tallyswiter@hotmail.com>
2023-08-16 23:35:06 -0300

    Criação Layout Inicial
    Desenvolvimento do layout inicial do projeto.
    Commit responsável pela organização do frontend.

---
+++ PetHelper-App/src/app/components/estoque/estoque.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estoque',
+  templateUrl: './estoque.component.html',
+  styleUrls: ['./estoque.component.scss']
+})
+export class EstoqueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ PetHelper-App/src/app/shared/const/nav-data.ts
@@ -0,0 +1,23 @@
+export const navbarData = [
+  {
+      routeLink: 'home',
+      icon: 'fal fa-home',
+      label: 'Tela Inicial'
+  },
+  {
+      routeLink: 'cliente',
+      icon: 'fal fa-address-card',
+      label: 'Clientes'
+  },
+  {
+    routeLink: 'servicos',
+    icon: 'fal fa-briefcase',
+    label: 'Serviços'
+  },
+  {
+      routeLink: 'estoque',
+      icon: 'fal fa-box-open',
+      label: 'Estoque'
+  },
+
+];
